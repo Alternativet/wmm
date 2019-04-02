0e57471ee07cee8f0f59aaee74f278ac0a7f19a4
Anders Nebbelunde <aeh.jakobsen@gmail.com>
2019-04-02 14:12:47 +0200

    cleanup

--- watson.py
+++ watson.py
@@ -1,6 +1,5 @@
 import numpy as np
 import datetime
-import time
 
 from scipy.special import gamma
 # References:
@@ -8,65 +7,70 @@ from scipy.special import gamma
 #   Maximum-likelihood estimation and other aspects,
 #   Journal of Multivariate Analysis 114 (2013) 256-269
 
-def pdf(X,mu,kappa):
-    (N,p) = X.shape
-    cp = gamma(p/2) / (2*np.pi**(p/2) * (kummer(1/2,p/2,kappa)))
-    return cp * np.exp(kappa * np.einsum('i,ji->j',mu,X)**2)
 
-def wmm_pdf(X,Mu,Kappa,Pi):
-    return np.sum([pi * pdf(X,mu,kappa) for mu,kappa,pi in zip(Mu,Kappa,Pi)],axis=0)
+def pdf(X, mu, kappa):
+    (N, p) = X.shape
+    cp = gamma(p/2) / (2*np.pi**(p/2) * (kummer(1/2, p/2, kappa)))
+    return cp * np.exp(kappa * np.einsum('i,ji->j', mu, X)**2)
 
-def wmm_fit(X,k,maxkappa=700,maxiter=200,tol=1e-4,verbose=False,seed=None,init=None):
+
+def wmm_pdf(X, Mu, Kappa, Pi):
+    return np.sum([pi * pdf(X, mu, kappa) for mu, kappa, pi in zip(Mu, Kappa, Pi)], axis=0)
+
+
+def wmm_fit(X, k, maxkappa=700, maxiter=200, tol=1e-4, verbose=False, seed=None, init=None):
     if seed is not None:
         np.random.seed(seed)
 
     # Get dimensions
-    (N,p) = X.shape
+    (N, p) = X.shape
 
     # Initialization
     if init is None:
         if p == 3:
-            mu,kappa,pi = golden_spiral_init(k)
+            mu, kappa, pi = golden_spiral_init(k)
         else:
-            mu,kappa,pi = random_init(k,X,N,p,maxkappa)
+            mu, kappa, pi = random_init(k, X, N, p, maxkappa)
     elif init == 'spiral':
         assert p == 3
-        mu,kappa,pi = golden_spiral_init(k)
+        mu, kappa, pi = golden_spiral_init(k)
     elif init == 'random':
-        mu,kappa,pi = random_init(k,X,N,p,maxkappa)
+        mu, kappa, pi = random_init(k, X, N, p, maxkappa)
 
     # Allocation
-    bounds = np.zeros((maxiter,k,3));
-    llh = np.zeros((maxiter));
-    num = np.zeros((N,k))
+    bounds = np.zeros((maxiter, k, 3))
+    llh = np.zeros((maxiter))
+    num = np.zeros((N, k))
 
     iter = 0
     converged = False
 
     # EM loop
     while not converged:
-        beta,llh[iter] = e_step(X,mu,kappa,pi,num,k,p)
-        mu,kappa,pi,bounds[iter,:] = m_step(X,mu,kappa,pi,beta,k,N,p,maxkappa)
-        converged = convergence(llh,iter,maxiter,tol,verbose)
+        beta, llh[iter] = e_step(X, mu, kappa, pi, num, k, p)
+        mu, kappa, pi, bounds[iter, :] = m_step(X, mu, kappa, pi, beta, k, N, p, maxkappa)
+        converged = convergence(llh, iter, maxiter, tol, verbose)
         iter += 1
 
     llh = llh[:iter]
     bounds = bounds[:iter]
-    return mu,kappa,pi,llh,bounds
+    return mu, kappa, pi, llh, bounds
 
-def random_init(k,X,N,p,maxkappa):
-    mu,kappa,pi = np.zeros((k,p)),np.zeros(k),np.zeros(k)
-    beta = np.random.rand(N,k)
-    beta = beta/np.sum(beta,axis=1)[:,np.newaxis]
-    mu,kappa,pi,_ = m_step(X,mu,kappa,pi,beta,k,N,p,maxkappa)
-    return mu,kappa,pi
+
+def random_init(k, X, N, p, maxkappa):
+    mu, kappa, pi = np.zeros((k, p)), np.zeros(k), np.zeros(k)
+    beta = np.random.rand(N, k)
+    beta = beta/np.sum(beta, axis=1)[:, np.newaxis]
+    mu, kappa, pi = m_step(X, mu, kappa, pi, beta, k, N, p, maxkappa)
+    return mu, kappa, pi
+
 
 def golden_spiral_init(k):
     # Initialise mu based on "The golden spiral method" https://stackoverflow.com/a/44164075/6843855
     indices = np.arange(0, k, dtype=float) + 0.5
     phi = np.arccos(1 - 2*indices/(2*k))
     theta = np.pi * (1 + 5**0.5) * indices
-    mu = np.stack((np.cos(theta) * np.sin(phi), np.sin(theta) * np.sin(phi), np.cos(phi)),axis=1)
+    mu = np.stack((np.cos(theta) * np.sin(phi), np.sin(theta) * np.sin(phi), np.cos(phi)), axis=1)
 
     # Initalize kappa
     kappa = np.ones(k)
@@ -74,78 +78,86 @@ def golden_spiral_init(k):
     # Initialize pi
     pi = np.ones(k)/k
 
-    return mu,kappa,pi
+    return mu, kappa, pi
 
-def e_step(X,mu,kappa,pi,num,k,p):
+
+def e_step(X, mu, kappa, pi, num, k, p):
     # Expectation
-    for j in range(k): # For each component
-        cp = gamma(p/2) / (2*np.pi**(p/2) * (kummer(1/2,p/2,kappa[j])))
+    for j in range(k):  # For each component
+        cp = gamma(p/2) / (2*np.pi**(p/2) * (kummer(1/2, p/2, kappa[j])))
         # Uses Watson distribution (2.1) [1], compute using 4.3 [1]
-        num[:,j] = pi[j] * cp * np.exp(kappa[j] * np.einsum('i,ji->j',mu[j,:],X)**2)
+        num[:, j] = pi[j] * cp * np.exp(kappa[j] * np.einsum('i,ji->j', mu[j, :], X)**2)
 
-    beta = num/np.sum(num,axis=1)[:,np.newaxis]
-    llh = np.sum(np.log(np.sum(num,axis=1)))
+    beta = num/np.sum(num, axis=1)[:, np.newaxis]
+    llh = np.sum(np.log(np.sum(num, axis=1)))
 
-    return beta,llh
+    return beta, llh
 
-def m_step(X,mu,kappa,pi,beta,k,N,p,maxkappa):
-    bounds = np.zeros((k,3))
+
+def m_step(X, mu, kappa, pi, beta, k, N, p, maxkappa):
+    bounds = np.zeros((k, 3))
 
     # Maximization
-    pi = np.sum(beta,axis=0)/N
-    for j in range(k): # For each component
+    pi = np.sum(beta, axis=0)/N
+    for j in range(k):  # For each component
         # Compute Sj (4.5)
-        Sj = np.einsum('i,ij,ik->jk',beta[:,j],X,X,optimize='greedy') / np.sum(beta[:,j])
+        Sj = np.einsum('i,ij,ik->jk', beta[:, j], X, X, optimize='greedy') / np.sum(beta[:, j])
         # Compute mu using (4.4) [1], ignoring negative kappa case
-        [w,v] = np.linalg.eig(Sj)
+        [w, v] = np.linalg.eig(Sj)
         idx = np.argmax(w)
-        mu[j,:] = np.real(v[:,idx])
+        mu[j, :] = np.real(v[:, idx])
         # Compute Kappa using (4.5) [1]
-        r = np.einsum('i,ji,j->',mu[j,:],Sj,mu[j,:]) # mu'*Sj*mu
+        r = np.einsum('i,ji,j->', mu[j, :], Sj, mu[j, :])  # mu'*Sj*mu
         r = 0.99 if r > 0.999 else r
-        kappa[j] = lower_bound(1/2,p/2,r)
+        kappa[j] = lower_bound(1/2, p/2, r)
         kappa[j] = maxkappa if kappa[j] > maxkappa else kappa[j]
-        bounds[j,:] = [lower_bound(1/2,p/2,r),bound(1/2,p/2,r),upper_bound(1/2,p/2,r)]
+        bounds[j, :] = [lower_bound(1/2, p/2, r), bound(1/2, p/2, r), upper_bound(1/2, p/2, r)]
 
-    return mu,kappa,pi,bounds
+    return mu, kappa, pi, bounds
 
-def convergence(llh,iter,maxiter,tol,verbose):
+
+def convergence(llh, iter, maxiter, tol, verbose):
     # Convergece
     if iter > 0 and abs((llh[iter] - llh[iter-1])/llh[iter-1]) < tol:
-        print_t('Conveged in {} iterations'.format(iter+1),verbose)
+        print_t('Conveged in {} iterations'.format(iter+1), verbose)
         return True
     elif iter >= maxiter-1:
         print_t('Did not converge in maximum iterations')
         return True
 
     if (iter+1) % 10 == 0:
-        print_t('Iteration: {:d}, relative llh change: {:.2g}'.format(iter+1,(llh[iter] - llh[iter-1])/llh[iter-1]),verbose)
+        print_t('Iteration: {:d}, relative llh change: {:.2g}'.format(iter+1, (llh[iter] - llh[iter-1])/llh[iter-1]), verbose)
 
     return False
 
+
 # Definition of bounds solutions for Kappa (3.7)(3.8)(3.9) [1]
-def lower_bound(a,c,r):
+def lower_bound(a, c, r):
     return (r*c-a)/(r*(1-r))*(1+(1-r)/(c-a))
 
-def bound(a,c,r):
-    return (r*c-a)/(2*r*(1-r))*(1+np.sqrt( 1+(4*(c+1)*r*(1-r)) / (a* (c-a)) ) )
 
-def upper_bound(a,c,r):
+def bound(a, c, r):
+    return (r*c-a)/(2*r*(1-r))*(1+np.sqrt(1+(4*(c+1)*r*(1-r)) / (a*(c-a))))
+
+
+def upper_bound(a, c, r):
     return (r*c-a)/(r*(1-r))*(1+r/a)
 
-def kummer(a,b,kappa,tol=1e-10):
+
+def kummer(a, b, kappa, tol=1e-10):
     term = a*kappa/b
-    f = 1+term;
-    j = 1;
+    f = 1+term
+    j = 1
     while abs(term) > tol:
-        j += 1;
-        a += 1;
-        b += 1;
-        term *= a*kappa/b/j;
-        f += term;
+        j += 1
+        a += 1
+        b += 1
+        term *= a*kappa/b/j
+        f += term
 
     return f
 
-def print_t(s,verbose=True):
+
+def print_t(s, verbose=True):
     if verbose:
-        print('{:%H:%M:%S}: {}'.format(datetime.datetime.now(),s))
+        print('{:%H:%M:%S}: {}'.format(datetime.datetime.now(), s))
